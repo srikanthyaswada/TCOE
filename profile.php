e5051ad1f2ec1c94b899f9b2b21417586b5c3c78
stoswebsolutions <stoswebsolutions@gmail.com>
2024-08-27 11:43:43 +0530

    profile design changed

--- profile.php
+++ profile.php
@@ -14,51 +14,27 @@ $uniqueId = $_SESSION['uniqueId'];
     <meta charset="UTF-8" />
     <title>Profile</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link
-        rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" />
-    <link
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
-        rel="stylesheet" />
-    <link
-        rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
-    <link
-        href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
-        rel="stylesheet" />
-    <link
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css"
-        rel="stylesheet" />
-    <link
-        href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css"
-        rel="stylesheet" />
-    <link rel="stylesheet" href="assets/css/application.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet" />
+    <link rel="stylesheet" href="assets/css/profile.css" />
 </head>
 
 <body>
     <header class="navbar navbar-expand-lg navbar-light bg-light p-0 sticky-top">
         <div class="container-fluid">
             <a class="navbar-brand" href="participant">
-                <img
-                    src="./assets/images/Tcoe_logo.jpg"
-                    alt="Logo"
-                    width="80"
-                    height="50" />
+                <img src="./assets/images/Tcoe_logo.jpg" alt="Logo" width="80" height="50" />
             </a>
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item dropdown">
-                    <a
-                        class="nav-link dropdown-toggle"
-                        href="#"
-                        id="profileDropdown"
-                        role="button"
-                        data-bs-toggle="dropdown"
-                        aria-expanded="false">
+                    <a class="nav-link dropdown-toggle" href="#" id="profileDropdown" role="button"
+                        data-bs-toggle="dropdown" aria-expanded="false">
                         <i class="bi bi-person-circle" style="font-size: 2rem"></i>
                     </a>
-                    <ul
-                        class="dropdown-menu dropdown-menu-end"
-                        aria-labelledby="profileDropdown">
+                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="profileDropdown">
                         <li><a class="dropdown-item" href="participant">Home</a></li>
                         <li><a class="dropdown-item" href="profile">Profile</a></li>
                         <li><a class="dropdown-item" href="applicantView">My Application</a></li>
@@ -69,43 +45,71 @@ $uniqueId = $_SESSION['uniqueId'];
         </div>
     </header>
     <div class="row g-0">
-        <div class="col-sm-6 m-auto">
-            <?php
-            $sql = "SELECT * FROM users WHERE uniqueId = ?";
-            $stmt = $conn->prepare($sql);
-            $stmt->bind_param("i", $uniqueId);
-            $stmt->execute();
-            $result = $stmt->get_result();
-            if ($result->num_rows > 0) {
-                $row = $result->fetch_assoc();
-            ?>
-                <div class="card p-5">
-                    <div class="">Personal</div>
-                    <div class="mt-3">Name<strong class="mx-5"><?= $row['fullname'] ?></strong></div>
-                    <div class="mt-3">Email<strong class="mx-5"><?= $row['email'] ?></strong></div>
-                    <div class="mt-3">I am<strong class="mx-5"><?= $row['categoryType'] ?></strong></div>
+        <div class="col-sm-6 m-auto mt-5">
+            <div class="profile-card">
+                <?php
+                $fullname = '';
+                $email = '';
+                $categoryType = '';
+                $categoryName = '';
+                $sql = "SELECT * FROM users WHERE uniqueId = ?";
+                $stmt = $conn->prepare($sql);
+                $stmt->bind_param("i", $uniqueId);
+                $stmt->execute();
+                $result = $stmt->get_result();
+                if ($result->num_rows > 0) {
+                    $row = $result->fetch_assoc();
+                    $fullname = $row['fullname'];
+                    $email = $row['email'];
+                    $categoryType = $row['categoryType'];
+                    $categoryName = $row['categoryName'];
+                } else {
+                    echo "<script>alert('No record found for ID');</script>";
+                }
+                ?>
+                <h3 class=" p-4">My Profile</h3>
+                <div class="bg">
+                    <h5 class="p-2">Personal Data</h5>
                 </div>
-            <?php
-            } else {
-                echo "<script>alert('No record found for ID');</script>";
-            }
-            ?>
-
-            <form method="post" action="changepassword.php">
-                <input type="hidden" name="uniqueId" value="<?php echo $uniqueId; ?>">
-                <div class="mt-2">
-                    <label for="password">Enter your old password:</label>
-                    <input type="password" class="form-control" placeholder="Old Password" name="password1" id="password1" required>
+                <div class="profile-section">
+                    <p><strong>Name</strong><br><?= $fullname ?></p>
+                    <p><strong>Email</strong><br><?= $email ?></p>
+                    <p><strong>Specialization</strong><br><?= $categoryType ?></p>
+                    <p><strong>Organization Name</strong><br><?= $categoryName ?></p>
                 </div>
-                <div class="mt-2">
-                    <label for="password">Enter your new password:</label>
-                    <input type="password" class="form-control" placeholder="New Password" name="password" id="password" required>
+                <div class="accordion" id="resetPasswordAccordion">
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingOne">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                                Reset Password
+                            </button>
+                        </h2>
+                        <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne"
+                            data-bs-parent="#resetPasswordAccordion">
+                            <div class="accordion-body">
+                                <form method="post" action="changepassword.php">
+                                    <input type="hidden" name="uniqueId" value="<?php echo $uniqueId; ?>">
+                                    <div class="mb-3">
+                                        <label for="password1" class="form-label">Enter Old Password</label>
+                                        <input type="password" class="form-control" name="password1" id="password1"
+                                            placeholder="**********">
+                                    </div>
+                                    <div class="mb-3">
+                                        <label for="password" class="form-label">Enter New Password</label>
+                                        <input type="password" class="form-control" name="password" id="password"
+                                            placeholder="**********">
+                                    </div>
+                                    <button type="submit" class="btnc  btn w-100 text-white">Change Password</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <input type="submit" class="btn btn-primary mt-3" value="Change Password">
-            </form>
-
+            </div>
         </div>
     </div>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script> -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js"></script>
 </body>
